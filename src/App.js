d3ef5ac9d35777a01b2578aaa0739d8929e10b26
1000mj <cjsaudwls12@gmail.com>
2023-02-21 22:01:03 +0900

    틀 만들기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useEffect, useRef } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <ul className="top-menu"><div></div>
+        <div className='menu-box'><li className="menu"><i class="fa-solid fa-folder-open"></i></li></div>
+        <hr/>
+        <div className='menu-box'><li className="menu"><i class="fa-solid fa-book"></i></li></div>
+        <hr/>
+        <div className='menu-box'><li className="menu"><i class="fa-solid fa-user"></i></li></div>
+        <hr/>
+        <div className='menu-box'><li className="menu"><i class="fa-solid fa-house"></i></li></div>
+        <hr/>
+        <div className='menu-box'><li className="menu top-name">Chun Myeong Jin</li></div>
+      </ul>
     </div>
+    <div className="Background-image">
+      <div className='title'>
+        <h1 className="title-text-0">@@@@
+        <div className='underline blinking'></div></h1>
+        <h1 className="title-text-1">개발자를 꿈꾸는</h1>
+        <h1 className="title-text-2">천명진 포트폴리오 입니다.</h1>
+        <h1 className="title-text-3 blinking-1">ㅣ</h1>
+      </div>
+    </div>
+    <div className='Profile'>
+      <div className='Prologo'><i class="fa-solid fa-user fa-5x"></i></div>
+    </div>
+    <div className='Skills'>
+      <div className='Slogo'><i class="fa-solid fa-book fa-5x"></i></div>
+    </div>
+    <div className='Pj'>
+      <div className='Plogo'><i class="fa-solid fa-folder-open fa-5x"></i></div>
+    </div>
+    </>
   );
 }
 
---
+++ src/click.scroll.js
@@ -0,0 +1,10 @@
+import './App.css';
+import React from 'react';
+
+function Clickscroll() {
+  <>
+
+  </>
+}
+
+export default Clickscroll;
