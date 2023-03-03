a6d44c8d3473eff409bc6b2e54b0bb6f946229bd
1000mj <cjsaudwls12@gmail.com>
2023-03-03 22:56:29 +0900

    클릭 스크롤기능 구현(완)

--- src/App.js
+++ src/App.js
@@ -1,35 +1,22 @@
 import './App.css';
 import React from 'react';
 import { useRef } from 'react';
-import useMoveScrool from './ClickScroll'
 
 
 
 function App() {
-  
-  const goodsTabs = {
-    0: useMoveScrool('Main'),
-    1: useMoveScrool('Profile'),
-    2: useMoveScrool('Skills'),
-    3: useMoveScrool('Pj')
-  };
-
   return (
     <>
-    {Array.from(goodsTabs).map((tab, index) => {
-      <div onClick={tab.onMoveToElement}>{tab.name}</div>
-    })}
-
     <div >
-      <div ref={goodsTabs[0].element} className="Background-image main" >
+      <div id='Home' className="Background-image main" >
         <ul className="top-menu">
             <div className='name-box'><li className="menu top-name">Cheon Myeong Jin</li></div>
           </ul>
         <div>
-          <div className='menu-box'><li className="side-menu"><i class="fa-solid fa-house"></i></li></div>
-          <div className='menu-box'><li className="side-menu"><i class="fa-solid fa-user"></i></li></div>
-          <div className='menu-box '><li className="side-menu"><i class="fa-solid fa-book"></i></li></div>
-          <div className='menu-box '><li className="side-menu"><i class="fa-solid fa-folder-open"></i></li></div>
+          <div className='menu-box'><li className="side-menu"><a href="#Home" class="fa-solid fa-house"></a></li></div>
+          <div className='menu-box'><li className="side-menu"><a href="#Profile" class="fa-solid fa-user"></a></li></div>
+          <div className='menu-box '><li className="side-menu"><a href="#Skills" class="fa-solid fa-book"></a></li></div>
+          <div className='menu-box '><li className="side-menu"><a href="#Pj" class="fa-solid fa-folder-open"></a></li></div>
         </div>
         <div className='title'>
           <h1 className="title-text-0">@@@@
@@ -39,13 +26,22 @@ function App() {
           <h1 className="title-text-3 blinking-1">ㅣ</h1>
         </div>
       </div>
-      <div ref={goodsTabs[1].element} className='Profile'>
+      <div className='section'>
+        Profile
+      </div>
+      <div id='Profile' className='Profile'>
         <div className='Prologo'><i class="fa-solid fa-user fa-5x"></i></div>
       </div>
-      <div ref={goodsTabs[2].element} className='Skills '>
+      <div className='section'>
+        Skills
+      </div>
+      <div id='Skills' className='Skills '>
         <div className='Slogo'><i class="fa-solid fa-book fa-5x"></i></div>
       </div>
-      <div ref={goodsTabs[3].element} className='Pj '>
+      <div className='section'>
+        Project
+      </div>
+      <div id='Pj' className='Pj '>
         <div className='Plogo'><i class="fa-solid fa-folder-open fa-5x"></i></div>
 
       </div>
