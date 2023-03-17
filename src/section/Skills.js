f1d10fe235365299bdd6f99c6231eb846affd995
1000mj <cjsaudwls12@gmail.com>
2023-03-17 22:21:55 +0900

    html,css,js,react 로고적용

--- src/section/Skills.js
+++ src/section/Skills.js
@@ -7,12 +7,25 @@ function Skills() {
         Skills
       </div>
       <div className="Skills">
-        <div className="Tring">Tring│Learning</div>
+        <div className="Tring">Tring Learning</div>
         <div className="skills-img">
           <div className="skills-ico">
+            <img src="https://i.ibb.co/wBHGJvq/css.png" alt="" border="0" />
+          </div>
+          <div className="skills-ico">
+            <img src="https://i.ibb.co/NKkTMyk/html.png" alt="" border="0" />
+          </div>
+          <div className="skills-ico">
             <img
-              src="https://i.postimg.cc/bdVbmhrG/css1.png"
-              alt=""
+              src="https://i.ibb.co/3CZGGQw/javascript.png"
+              alt="javascript"
+              border="0"
+            />
+          </div>
+          <div className="skills-ico">
+            <img
+              src="https://i.ibb.co/b2hV55B/react.png"
+              alt="react"
               border="0"
             />
           </div>
