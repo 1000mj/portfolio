072df1e5f2a3b2a35af36ad55fe2242cb5a82d07
1000mj <cjsaudwls12@gmail.com>
2023-04-10 22:27:06 +0900

    profile

--- src/popup/topname-pu.js
+++ src/popup/topname-pu.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import * as ReactDOM from "react-dom";
 
 function Topname(props) {
--- src/section/Profile.js
+++ src/section/Profile.js
@@ -9,6 +9,7 @@ function Profile() {
       <div className="Profile">
         <div className="Prologo">
           <i className="fa-solid fa-user fa-5x"></i>
+          <div>자기소개 구간</div>
         </div>
       </div>
     </>
