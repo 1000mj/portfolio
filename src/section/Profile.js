32c94fe51ef9964e9bd80994c68c8d11061ffb0e
1000mj <cjsaudwls12@gmail.com>
2023-04-27 21:17:11 +0900

    프로필

--- src/section/Profile.js
+++ src/section/Profile.js
@@ -11,8 +11,8 @@ function Profile() {
           <i className="fa-solid fa-user fa-5x"></i>
         </div>
         <div className="p-section">
-          <h1>ㅇㅇㅇ</h1>
-          <div className="">ㅇㅇㅇ</div>
+          <h1>더 나은 미래와 내일을 위해 노력한다.</h1>
+          <div className="intro">안녕하세요, 프론트엔드 개발자 지망생입니다. 비전공자 출신이지만 항상 새로운 기술과 트렌드에 관심을 가지고 배움을 게을리하지 않습니다. 문제 해결 능력과 소통을 중요시하며, 일하게 될 사람들과 함께 더 나은 사용자 경험을 제공하기 위해 최선을 다하겠습니다.</div>
         </div>
       </div>
 
