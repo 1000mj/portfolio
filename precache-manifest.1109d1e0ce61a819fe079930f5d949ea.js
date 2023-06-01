2c510afe192f75c10c84ebd1d9a806c32ac0c99d
1000mj <cjsaudwls12@gmail.com>
2023-06-01 16:43:46 +0900

    Updates

---
+++ precache-manifest.1109d1e0ce61a819fe079930f5d949ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6b980f9a118b4baba13",
+    "url": "/portfolio/static/css/main.768cad15.chunk.css"
+  },
+  {
+    "revision": "b6b980f9a118b4baba13",
+    "url": "/portfolio/static/js/main.b6b980f9.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/portfolio/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "2fe18daec77087e911c0",
+    "url": "/portfolio/static/js/2.2fe18dae.chunk.js"
+  },
+  {
+    "revision": "5283b6ae167cefcd5d66",
+    "url": "/portfolio/static/js/runtime~main.5283b6ae.js"
+  },
+  {
+    "revision": "eaa0b3d7af1541822d268e73c60dc117",
+    "url": "/portfolio/static/media/background.eaa0b3d7.jpg"
+  },
+  {
+    "revision": "77f71631fe1f1444abae7e9c8b4d3753",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MovieCloen_app/precache-manifest.bce2cb0aba5d77fff180acd88f3c5099.js"
+  "/portfolio/precache-manifest.1109d1e0ce61a819fe079930f5d949ea.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/MovieCloen_app/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
