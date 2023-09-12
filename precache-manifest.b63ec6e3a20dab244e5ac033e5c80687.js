41a7e711e0145dc6f82ef82e05be7444e8f8b434
천명진 <cjsaudwls12@gmail.com>
2023-09-12 17:19:14 +0900

    Updates

---
+++ precache-manifest.b63ec6e3a20dab244e5ac033e5c80687.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "68defb328d947a02dce9",
+    "url": "/portfolio/static/css/main.10134f08.chunk.css"
+  },
+  {
+    "revision": "68defb328d947a02dce9",
+    "url": "/portfolio/static/js/main.68defb32.chunk.js"
+  },
+  {
+    "revision": "444f0bc38a785630c6eb",
+    "url": "/portfolio/static/js/1.444f0bc3.chunk.js"
+  },
+  {
+    "revision": "742b9ed2d794a453fd8d",
+    "url": "/portfolio/static/js/2.742b9ed2.chunk.js"
+  },
+  {
+    "revision": "f09c399266580b3532b9",
+    "url": "/portfolio/static/js/runtime~main.f09c3992.js"
+  },
+  {
+    "revision": "eaa0b3d7af1541822d268e73c60dc117",
+    "url": "/portfolio/static/media/background.eaa0b3d7.jpg"
+  },
+  {
+    "revision": "781d1183f034ce967629c3a8ddd9c09f",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.fccdff065b2bda2a8eb2dbbab96bd6ba.js"
+  "/portfolio/precache-manifest.b63ec6e3a20dab244e5ac033e5c80687.js"
 );
 
 workbox.clientsClaim();
